f3ddba268851a6bdc4849b7eb958872fe981b1f9
AntonioOliveiraa <antoniooliveiraantonio-augusto@outlook.com>
2024-08-08 22:07:10 -0300

    Objeto novaTransação

--- js/bytebank.js
+++ js/bytebank.js
@@ -1,7 +1,7 @@
 let saldo = 3000;
 
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
-elementoSaldo.textContent = `R$${saldo}`;
+elementoSaldo.textContent = saldo;
 
 const elementoFormulario = document.querySelector(".block-nova-transacao form");
 elementoFormulario.addEventListener("submit", function(event){
@@ -10,4 +10,34 @@ elementoFormulario.addEventListener("submit", function(event){
         alert("Por favor, preencha todos os dados da transação!");
         return
     }
+
+    const inputTipoTransação = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+
+    let tipoTransacao = inputTipoTransação.value;
+    let valor = inputValor.value;
+    let data = inputData.value;
+
+    if (tipoTransacao == "Depósito") {
+        saldo += parseFloat(valor);
+    } else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto"){
+        saldo -= parseFloat(valor);
+    } else {
+        alert("Transação inválida!");
+        return
+    }
+
+    elementoSaldo.textContent = saldo;
+
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: parseFloat(valor),
+        data: data
+    }
+
+    console.log(novaTransacao);
+    elementoFormulario.reset();
+
 })
+
